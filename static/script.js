359114b46c97605bc879a478dc1e17b7b34d24c2
Zeena Mohamed <zkm2106@barnard.edu>
2025-04-23 00:53:27 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,3 +3,23 @@ function checkAnswer(isYes) {
     // Placeholder logic
     feedback.textContent = isYes ? "Correct!" : "Wrong!";
 }
+function submitAnswer(choice, qnum) {
+    fetch("/submit_answer", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            answer: choice,
+            question_number: qnum
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.next_question !== null) {
+            window.location.href = "/quiz/" + data.next_question;
+        } else {
+            window.location.href = "/quiz_result";
+        }
+    });
+}
